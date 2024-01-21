23e2a50fa9060ff95eeca6be500e4c1c8e2ceaf4
vuhaichieu <chieuvhhe151117@fpt.edu.vn>
2024-01-22 01:13:06 +0700

    add Logout

--- src/main/java/Controllers/LogoutController.java
+++ src/main/java/Controllers/LogoutController.java
@@ -1,5 +1,7 @@
 package Controllers;
 
+import DataAccess.SessionManagerRepository;
+import Utils.Constants.UserConstant;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,6 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @WebServlet(name = "LogoutController", urlPatterns = "/logout")
 public class LogoutController extends HttpServlet{
@@ -15,6 +18,13 @@ public class LogoutController extends HttpServlet{
         // Invalidate the session (log out the user)
         HttpSession session = request.getSession(false);
         if (session != null) {
+            UUID userId = (UUID) session.getAttribute(UserConstant.SESSION_USERID);
+            String sessionId = session.getId();
+            // Perform any additional cleanup actions if needed
+            SessionManagerRepository sessionManagerRepository = new SessionManagerRepository();
+
+            // Delete the session data from the database using the retrieved user ID
+            sessionManagerRepository.removeSession(sessionId, userId);
             session.invalidate();
         }
 
@@ -24,6 +34,7 @@ public class LogoutController extends HttpServlet{
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Handle GET request, e.g., show a confirmation page
-        request.getRequestDispatcher("/pages/logoutConfirmation.jsp").forward(request, response);
+        request.getRequestDispatcher("/pages/login.jsp").forward(request, response);
     }
+
 }
