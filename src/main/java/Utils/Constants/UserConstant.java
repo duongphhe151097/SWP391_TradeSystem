7953e993adcc962b802af604e30b0607043ebce6
Minh <minhndhe171146@fpt.edu.vn>
2024-01-24 12:56:57 +0700

    Create UserConstant.java

---
+++ src/main/java/Utils/Constants/UserConstant.java
@@ -0,0 +1,12 @@
+package Utils.Constants;
+
+public class UserConstant {
+    public static short PENDING = 0;
+    public static short ACTIVE = 1;
+    public static short LOCKED = 2;
+    public static short BANNED = 3;
+    public static String SESSION_USERID = "SESSION_USERID";
+    public static String SESSION_USERNAME = "SESSION_USERNAME";
+    public static String SESSION_USEREMAIL = "SESSION_USEREMAIL";
+    public static String SESSION_USERFULLNAME = "SESSION_USERFULLNAME";
+}
