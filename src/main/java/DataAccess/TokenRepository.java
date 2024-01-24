c5901d284f170a9433775c4839b242e38a04dcce
Minh <minhndhe171146@fpt.edu.vn>
2024-01-24 20:20:53 +0700

    Delete TokenRepository.java

--- src/main/java/DataAccess/TokenRepository.java
+++
@@ -1,9 +0,0 @@
-package DataAccess;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TokenRepository extends JpaRepository<Token, Long> {
-
-    Token findByTokenAndStatus(String token, int status);
-}
-
