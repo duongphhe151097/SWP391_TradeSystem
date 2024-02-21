cd763ca0b19f09137587526e06cd3a9e340c2645
vuhaichieu <chieuvhhe151117@fpt.edu.vn>
2024-02-21 13:22:16 +0700

    update User trans

--- src/main/java/Controllers/UserExternalTransaction.java
+++ src/main/java/Controllers/UserExternalTransaction.java
@@ -25,55 +25,55 @@ import java.util.UUID;
 @Authorization(role = "", isPublic = false)
 public class UserExternalTransaction extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session = req.getSession(false);
-        if (session != null) {
-            UUID userId = (UUID) session.getAttribute(UserConstant.SESSION_USERID);
-            String sessionId = session.getId();
-            // Perform any additional cleanup actions if needed
-            TransactionManagerRepository transactionManagerRepository = new TransactionManagerRepository()
-
-            transactionManagerRepository.getExternalTransactionByUser(userId);
-            session.invalidate();
-
-        TransactionManagerRepository transactionManagerRepository = new TransactionManagerRepository();
-
-        // Lấy thông tin từ yêu cầu
-        String currentPage = req.getParameter("current");
-        String pageSize = req.getParameter("size");
-        String pageRange = req.getParameter("range");
-
-        try {
-            if (StringValidator.isNullOrBlank(currentPage)
-                    || StringValidator.isNullOrBlank(pageSize)
-                    || StringValidator.isNullOrBlank(pageRange)) {
-                currentPage = "1";
-                pageSize = "5";
-                pageRange = "5";
-            }
-
-
-
-
-
-            long userCount = transactionManagerRepository.countAll();
-            Pagination pagination
-                    = new Pagination(userCount, Integer.parseInt(currentPage), Integer.parseInt(pageRange), Integer.parseInt(pageSize));
-
-            int startPage = (pagination.getCurrentPage() - 1) * pagination.getPageSize();
-            int endPage = pagination.getPageSize();
-            List<ExternalTransactionEntity> externalTransactions = transactionManagerRepository
-                    .getExternalTransactionsWithPaging(startPage, endPage);
-
-            req.setAttribute("VIEW_PAGING", new ViewPaging<>(externalTransactions, pagination));
-
-            req.getRequestDispatcher("/pages/Transactions/ExternalTransactions.jsp").forward(req, resp);
-        } catch (Exception e) {
-            e.printStackTrace();
-            Pagination pagination
-                    = new Pagination(0, 1, 5, 10);
-            req.setAttribute("VIEW_PAGING", new ViewPaging<UserEntity>(new ArrayList<>(), pagination));
-        }
-
+//        HttpSession session = req.getSession(false);
+//        if (session != null) {
+//            UUID userId = (UUID) session.getAttribute(UserConstant.SESSION_USERID);
+//            String sessionId = session.getId();
+//            // Perform any additional cleanup actions if needed
+//            TransactionManagerRepository transactionManagerRepository = new TransactionManagerRepository()
+//
+//            transactionManagerRepository.getExternalTransactionByUser(userId);
+//            session.invalidate();
+//
+//        TransactionManagerRepository transactionManagerRepository = new TransactionManagerRepository();
+//
+//        // Lấy thông tin từ yêu cầu
+//        String currentPage = req.getParameter("current");
+//        String pageSize = req.getParameter("size");
+//        String pageRange = req.getParameter("range");
+//
+//        try {
+//            if (StringValidator.isNullOrBlank(currentPage)
+//                    || StringValidator.isNullOrBlank(pageSize)
+//                    || StringValidator.isNullOrBlank(pageRange)) {
+//                currentPage = "1";
+//                pageSize = "5";
+//                pageRange = "5";
+//            }
+//
+//
+//
+//
+//
+//            long userCount = transactionManagerRepository.countAll();
+//            Pagination pagination
+//                    = new Pagination(userCount, Integer.parseInt(currentPage), Integer.parseInt(pageRange), Integer.parseInt(pageSize));
+//
+//            int startPage = (pagination.getCurrentPage() - 1) * pagination.getPageSize();
+//            int endPage = pagination.getPageSize();
+//            List<ExternalTransactionEntity> externalTransactions = transactionManagerRepository
+//                    .getExternalTransactionsWithPaging(startPage, endPage);
+//
+//            req.setAttribute("VIEW_PAGING", new ViewPaging<>(externalTransactions, pagination));
+//
+//            req.getRequestDispatcher("/pages/Transactions/ExternalTransactions.jsp").forward(req, resp);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            Pagination pagination
+//                    = new Pagination(0, 1, 5, 10);
+//            req.setAttribute("VIEW_PAGING", new ViewPaging<UserEntity>(new ArrayList<>(), pagination));
+//        }
+//
+//    }
     }
-
 }
