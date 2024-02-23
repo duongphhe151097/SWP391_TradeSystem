e72b91da7f7ef825fe47451c8c518cd7303405f3
Minh <minhndhe171146@fpt.edu.vn>
2024-02-23 07:42:19 +0700

    add controller for purchase order

---
+++ src/main/java/Controllers/purchaseOrderController.java
@@ -0,0 +1,31 @@
+package Controllers;
+
+import DataAccess.ProductRepository;
+import Utils.Annotations.Authorization;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@WebServlet(name = "purchaseOrderController", urlPatterns = "/purchaseOrder")
+@Authorization(role = "", isPublic = false)
+public class purchaseOrderController extends BaseController {
+
+    private ProductRepository productRepository;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UUID orderId = UUID.fromString(request.getParameter("orderId"));
+        UUID userId = (UUID) request.getSession().getAttribute("userId");
+
+        ProductRepository productRepository = new ProductRepository();
+        try {
+            productRepository.purchaseOrder(orderId, userId);
+            response.sendRedirect("purchaseSuccess.jsp");
+        } catch (IllegalArgumentException e) {
+            response.sendRedirect("purchaseError.jsp?message=" + e.getMessage());
+        }
+    }
+}
