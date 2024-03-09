f739dd3a2707c77f864b7f8d92581da0f276657a
duongphhe151097 <duongphhe151097@fpt.edu.vn>
2024-03-10 04:30:21 +0700

    fix: deploy settings file bug

--- deploy_settings/common.js
+++ deploy_settings/common.js
@@ -5,11 +5,55 @@ const urlBuilder = (router) => {
 }
 
 const formatCurrency = (value) => {
-    try {
-        return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(value)
-    } catch (err) {
-        return 0;
+    return value.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })
+}
+
+const formatVnPayTime = (value) => {
+    const year = value.substring(0, 4);
+    const month = value.substring(4, 6);
+    const day = value.substring(6, 8);
+    const hour = value.substring(8, 10);
+    const minute = value.substring(10, 12);
+    const second = value.substring(12, 14);
+
+    return `${day}/${month}/${year} ${hour}:${minute}:${second}`;
+}
+
+const formatVnPayTxnRef = (value) => {
+    const data = [
+        value.substring(0, 8),
+        value.substring(8, 12),
+        value.substring(12, 16),
+        value.substring(16, 20),
+        value.substring(20)
+    ];
+
+    // Join the groups with hyphens
+    return data.join('-');
+}
+
+const formatVnPayStatus = (value) => {
+    switch (value) {
+        case "00":
+            return "Thành công"
+
+        case "01":
+            return "Không thành công"
+
+        case "08":
+            return "Không thành công"
+
+        default:
+            return "Không rõ"
     }
 }
 
-export {urlBuilder, formatCurrency, BASE_URL}
+export {
+    urlBuilder,
+    formatVnPayTime,
+    formatVnPayTxnRef,
+    formatVnPayStatus,
+    formatCurrency,
+    BASE_URL
+}
+
