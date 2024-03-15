f7d6178620f8faaa3f23017034cfc98542e5385f
Minh <minhndhe171146@fpt.edu.vn>
2024-03-15 16:58:10 +0700

    functions work w categories

---
+++ src/main/java/DataAccess/CategoryRepository.java
@@ -0,0 +1,53 @@
+package DataAccess;
+
+import Models.CategoryEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+
+import java.util.List;
+
+public class CategoryRepository {
+    private SessionFactory sessionFactory;
+
+    public CategoryRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public List<CategoryEntity> getAllCategories() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM category", CategoryEntity.class).list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public void addCategory(CategoryEntity category) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.save(category);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isIdExists(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Long> query = session.createQuery("SELECT COUNT(*) FROM category WHERE id = :id", Long.class);
+            query.setParameter("id", id);
+            Long count = query.uniqueResult();
+            return count != null && count > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
